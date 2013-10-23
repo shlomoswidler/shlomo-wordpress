12511f866abf3727b8bd302af02a383494eecab8
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-23 23:02:57 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      "Installs/Configures WordPress"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.1"
 
-recipe "WordPress", "Installs and configures WordPress LAMP stack on a single system"
+recipe "WordPress", "Installs and configures WordPress, and optionally the MySQL database it will use"
 recipe "WordPress::languages", "Install WordPress translation files"
 
 %w{ php openssl }.each do |cb|
@@ -15,7 +15,6 @@ end
 
 depends "apache2", ">= 0.99.4"
 depends "mysql", ">= 1.0.5"
-depends "build-essential"
 
 %w{ debian ubuntu }.each do |os|
   supports os
