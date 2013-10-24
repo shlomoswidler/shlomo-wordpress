1a7a3c5d2fb12c31d293920268f82aa64a6b79c7
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-24 13:28:21 +0300

    Create secure_password.rb

---
+++ libraries/secure_password.rb
@@ -0,0 +1,15 @@
+# Monkey-copied from openssl cookbook
+# because for some reason the recipes in this cookbook can't see that one
+# Yes, the metadata is configured properly.
+
+require 'openssl'
+
+module SecurePassword
+  def self.secure_password(length = 20)
+    pw = String.new
+
+    while pw.length < length
+      pw << ::OpenSSL::Random.random_bytes(1).gsub(/\W/, '')
+    end
+  pw
+end
