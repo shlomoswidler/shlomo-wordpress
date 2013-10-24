1b9df536836e1b6b014f5fbbe991faa38cc6536f
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-10-25 02:29:44 +0300

    Update default.rb
    correct quotes!

--- recipes/default.rb
+++ recipes/default.rb
@@ -103,7 +103,7 @@ template "#{node['mysql']['conf_dir']}/wp-grants.sql" do
 end
 
 execute "create #{node['wordpress']['db']['database']} database" do
-  command "/usr/bin/mysql -h #{node['wordpress']['db']['host']} -u #{node['mysql']['server_root_user']} -p\"#{node['mysql']['server_root_password']}\" create database if not exists #{node['wordpress']['db']['database']}"
+  command "/usr/bin/mysql -h #{node['wordpress']['db']['host']} -u #{node['mysql']['server_root_user']} -p\"#{node['mysql']['server_root_password']}\" -e \"create database if not exists #{node['wordpress']['db']['database']}\""
   notifies :create, "ruby_block[save node data]", :immediately unless Chef::Config[:solo]
 end
 
