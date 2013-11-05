8fd3fed7c06a6ee2756f887de52747c451702936
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-05 02:52:43 +0200

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,7 @@ version          "0.1.0"
 recipe "WordPress", "Installs and configures WordPress, and optionally the MySQL database it will use"
 
 depends "wordpress", ">= 1.2.1"
+depends "awscli", "= 0.1.0"
 
 %w{ debian ubuntu }.each do |os|
   supports os
@@ -24,4 +25,18 @@ attribute "WordPress/wp_config_extras",
   :display_name => "Extra wp-config.php entries",
   :description => "hash of option name and value to place into wp-config.php, e.g. { 'ENABLE_CACHE' : 'false' }",
   :type => "string",
+  :default => nil
+
+attribute "WordPress/web_root_overlay_bundle/s3_url",
+  :display_name => "webroot overlay bundle",
+  :description => "S3 URL (s3://bucket/filename.tar.gz) of tarball to unpack into the wordpress root dir.",
+  :type => "string",
+  :required => "optional",
+  :default =>nil
+  
+attribute "WordPress/web_root_overlay_bundle/region",
+  :display_name => "region",
+  :description => "AWS region name in which the S3 bucket containing the webroot overlay bundle is located.",
+  :type => "string",
+  :required => "optional",
   :default => nil
