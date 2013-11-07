48daab1706e4ff7a84f2aed03a5fc02f07035ba1
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-07 11:13:31 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ include_attribute "wordpress"
 
 default['wordpress']['db']['host'] = "localhost"
 default['wordpress']['wp_config_extras'] = {}
-default['wordpress']['admin_ips'] = "all"
+default['wordpress']['admin_ips'] = ["all"]
