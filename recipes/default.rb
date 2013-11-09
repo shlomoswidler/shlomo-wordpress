0e47c039445e1b2c96562c1867bd1500117c0266
Shlomo Swidler <shlomo.swidler@gmail.com>
2013-11-10 01:05:54 +0200

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,14 @@
 wordpress_site node['wordpress']['app_name'] do
-  db              node['wordpress']['db']
+  repourl         node['wordpress']['repourl']
+  checksum        node['wordpress']['checksum']
+  version         node['wordpress']['version']
   dir             node['wordpress']['dir']
-  fqdn            node['wordpress']['fqdn']
   server_aliases  node['wordpress']['server_aliases']
+  fqdn            node['wordpress']['fqdn']
+  db              node['wordpress']['db']
+  table_prefix    node['wordpress']['table_prefix']
   languages       node['wordpress']['languages']
   wp_config_extras node['wordpress']['wp_config_extras']
-  table_prefix    node['wordpress']['table_prefix']
   web_root_overlay_bundle node['wordpress']['web_root_overlay_bundle']
   admin_ips       node['wordpress']['admin_ips']
 end
