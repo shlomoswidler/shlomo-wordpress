8361c819c07037bca89e4ba3c07b873bb99df24b
Shlomo Swidler <shlomo.swidler@gmail.com>
2014-07-14 01:43:55 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ version          "0.1.0"
 
 recipe "WordPress", "Installs and configures WordPress, and optionally the MySQL database it will use"
 
-depends "mysql", ">= 1.0.5"
+depends "mysql", "~> 1.0.0"
 depends "awscli", ">= 0.2.0"
 
 
